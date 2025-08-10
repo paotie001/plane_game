003ab3dd2d8913283479124bf52c3bdf6d6e1903
pao <phucthai797@gmail.com>
2025-08-10 23:28:10 +0700

    pre audio(kinda completed)

--- main.py
+++ main.py
@@ -5,28 +5,46 @@ from random import randint,choice
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image=pygame.image.load('graphics/player.png').convert_alpha()
-        self.image=pygame.transform.rotozoom(self.image,0,0.5)
-        self.rect=self.image.get_rect(center=(80,300))
+        self.original_image = pygame.image.load('graphics/player.png').convert_alpha()
+        self.original_image = pygame.transform.rotozoom(self.original_image, 0, 0.5)
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(80, 300))
         self.mask = pygame.mask.from_surface(self.image)
-        self.gravity=0
-        self.can_jump=True
+        self.gravity = 0
+        self.can_jump = True
+        self.angle = 0
 
     def jump(self):
         key = pygame.key.get_pressed()
         if key[pygame.K_SPACE] and self.can_jump:
+            self.angle += 34
             self.gravity -= 17
             self.can_jump = False
             pygame.time.set_timer(jump_delay, 300)
+
     def earthing(self):
         self.gravity += 0.5
         self.rect.y += self.gravity
+
     def reseting(self):
         self.rect.center = (80, 300)
-        self.gravity=0
+        self.gravity = 0
+        self.angle = 0
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.can_jump = True
+
+    def angleing(self):
+        if self.angle > -90:
+            self.angle -= 1
+        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 1)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.mask = pygame.mask.from_surface(self.image)
+
     def update(self):
         self.jump()
         self.earthing()
+        self.angleing()
 
 
 class Object(pygame.sprite.Sprite):
@@ -56,12 +74,12 @@ def coslision():
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
     score_surf = text_font.render(f'Score: {current_time}', False, (64, 64, 64))
-    score_rect = score_surf.get_rect(center=(250,20))
+    score_rect = score_surf.get_rect(center=(225,20))
     screen.blit(score_surf, score_rect)
     return current_time
 
 pygame.init()
-screen=pygame.display.set_mode((500,600))
+screen=pygame.display.set_mode((450,600))
 pygame.display.set_caption('FLAPPY BIRD')
 clock=pygame.time.Clock()
 game_active=False
@@ -77,14 +95,17 @@ obj_gr=pygame.sprite.Group()
 player.add(Player())
 
 # Intro
-game_name = text_font.render('Pilot exam', False, (0,0,0))
-game_name_rect = game_name.get_rect(center=(250, 250))
+game_name = text_font.render('PLANE GAME', False, (0,0,0))
+game_name_rect = game_name.get_rect(center=(225,100))
 
-game_message = text_font.render('Press space to start the exam', False, (0,0,0))
-game_message_rect = game_message.get_rect(center=(250,350 ))
+game_message = text_font.render('Press space to play', False, (0,0,0))
+game_message_rect = game_message.get_rect(center=(225,150 ))
+
+ap_background=pygame.image.load('graphics/intro2.png')
+ap_background=pygame.transform.rotozoom(ap_background,0,0.5)
 
 died=pygame.image.load('graphics/died.jpeg')
-died=pygame.transform.rotozoom(died,0,0.5)
+died=pygame.transform.rotozoom(died,0,0.52)
 # Timer
 jump_delay=pygame.USEREVENT+1
 spawn_timer=pygame.USEREVENT+2
@@ -106,6 +127,7 @@ while True:
                 game_active = True
                 start_time = int(pygame.time.get_ticks()/1000)
                 death = randint(2, 250)
+                obj_gr.empty()
                 player.sprite.reseting()
 
 
@@ -125,9 +147,9 @@ while True:
         
     else:
         score_board=text_font.render(f'You flew {score}km and kill {death} people',False,'black',)
-        score_board_rect=score_board.get_rect(center=(250,300))
+        score_board_rect=score_board.get_rect(center=(225,300))
         if score==0:
-            screen.fill('#4181e8')
+            screen.blit(ap_background,(0,0))
             screen.blit(game_name,game_name_rect)
             screen.blit(game_message,game_message_rect)
         else:
